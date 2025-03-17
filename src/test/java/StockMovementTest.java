343fc6a463180bd57b509d98f18f2b6d4ad58b00
Radiant <hei.taratra@gmail.com>
2025-03-17 21:06:47 +0300

    test(stockMovement): added test for stockMovement

--- src/test/java/StockMovementTest.java
+++ src/test/java/StockMovementTest.java
@@ -1,5 +1,3 @@
-import lombok.Cleanup;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
