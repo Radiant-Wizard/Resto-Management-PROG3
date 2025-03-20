5e02d34c001877ffc0bea9cb829aaf4c644b2b00
Radiant <hei.taratra@gmail.com>
2025-03-20 17:06:50 +0300

    chore(migration): made foreign delete on cascade

--- src/main/java/org/radiant_wizard/db/migration/6_orders_dish_table_migration.sql
+++ src/main/java/org/radiant_wizard/db/migration/6_orders_dish_table_migration.sql
@@ -1,8 +1,7 @@
 CREATE TABLE if not exists order_dish(
     order_dish_id BIGSERIAL PRIMARY KEY,
     dish_id BIGINT references dishes(dish_id) NOT NULL,
-    order_id BIGINT references orders(order_id) NOT NULL,
-    ordered_dish_date TIMESTAMP without time zone DEFAULT CURRENT_TIMESTAMP,
+    order_id BIGINT references orders(order_id) NOT NULL  ON DELETE CASCADE,
     ordered_dish_quantity INT NOT NULL
 );
 
