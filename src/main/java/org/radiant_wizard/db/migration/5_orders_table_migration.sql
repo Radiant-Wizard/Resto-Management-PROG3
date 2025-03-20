6d845217b80a475037dc5dd8de39998d0537862c
Radiant <hei.taratra@gmail.com>
2025-03-20 17:06:11 +0300

    chore(orders): made order_reference UNIQUE

--- src/main/java/org/radiant_wizard/db/migration/5_orders_table_migration.sql
+++ src/main/java/org/radiant_wizard/db/migration/5_orders_table_migration.sql
@@ -2,5 +2,5 @@ create type statusType as ENUM('CREATED', 'CONFIRMED', 'IN_PROGRESS', 'FINISHED'
 
 CREATE TABLE if not exists orders(
     order_id BIGSERIAL PRIMARY KEY,
-    order_reference varchar(15)
+    order_reference varchar(15) UNIQUE
 );
