bb1ccc621a6fcbdec0df9d3e9573e62a128721a1
Radiant <hei.taratra@gmail.com>
2025-03-24 11:17:00 +0300

    chore: created order and dishOrder entities

---
+++ src/main/java/org/radiant_wizard/Entity/DishOrder.java
@@ -0,0 +1,30 @@
+package org.radiant_wizard.Entity;
+
+import lombok.Data;
+import org.radiant_wizard.Entity.Enum.StatusType;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Data
+public class DishOrder {
+    private final long dishOrderId;
+    private final Dish commendedDish;
+    private final Integer dishQuantityCommanded;
+    private List<Status> statusList;
+
+    public DishOrder(long dishOrderId, Dish commendedDish, Integer dishQuantityCommanded) {
+        this.dishOrderId = dishOrderId;
+        this.commendedDish = commendedDish;
+        this.dishQuantityCommanded = dishQuantityCommanded;
+    }
+
+    public StatusType getActualStatus(){
+        return statusList
+                .stream()
+                .max(Comparator.comparing(Status::getCreationDate))
+                .map(Status::getStatusType)
+                .orElse(null);
+    }
+}
+
