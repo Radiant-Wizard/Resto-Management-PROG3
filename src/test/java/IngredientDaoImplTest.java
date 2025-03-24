d992ffdf26e97c2782ec7068548885c201e15fb7
Radiant <hei.taratra@gmail.com>
2025-03-24 11:19:36 +0300

    test(IngredientDao): added more test to test the logical operator

--- src/test/java/IngredientDaoImplTest.java
+++ src/test/java/IngredientDaoImplTest.java
@@ -2,8 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.radiant_wizard.Entity.Ingredient;
-import org.radiant_wizard.Entity.LogicalOperator;
-import org.radiant_wizard.Entity.Unit;
+import org.radiant_wizard.Entity.Enum.LogicalOperator;
 import org.radiant_wizard.dao.IngredientDaoImpl;
 import org.radiant_wizard.db.Criteria;
 import org.radiant_wizard.db.Datasource;
@@ -24,11 +23,11 @@ public class IngredientDaoImplTest {
     @Test
     public void testGetIngredientByCriteria() throws SQLException {
         List<Criteria> criteriaList = new ArrayList<>();
-        criteriaList.add(new Criteria("ingredient_name", "Tomato", "LIKE", LogicalOperator.AND));
-        criteriaList.add(new Criteria("unit_price", 10, ">", LogicalOperator.AND));
+        criteriaList.add(new Criteria("ingredient_name", "oil", "LIKE", LogicalOperator.AND));
+//        criteriaList.add(new Criteria("unit_price", 10, ">", LogicalOperator.AND));
 
         List<Ingredient> ingredients = ingredientDao.getIngredientByCriteria(criteriaList, null, null, 2,  1);
 
-        Assertions.assertEquals(2, ingredients.size());
+        Assertions.assertEquals(1, ingredients.size());
     }
 }
